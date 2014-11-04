2973d3c9f78f0905f09472c8e6adfed994096e77
Matt Linaberry <matt.linaberry@we-ics.com>
2014-11-04 16:56:33 -0500

    Initial Commit

---
+++ NSUserDefaults, Segues and Protocols Challenge SolutionTests/NSUserDefaults__Segues_and_Protocols_Challenge_SolutionTests.m
@@ -0,0 +1,40 @@
+//
+//  NSUserDefaults__Segues_and_Protocols_Challenge_SolutionTests.m
+//  NSUserDefaults, Segues and Protocols Challenge SolutionTests
+//
+//  Created by Matthew Linaberry on 11/4/14.
+//  Copyright (c) 2014 Matthew Linaberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface NSUserDefaults__Segues_and_Protocols_Challenge_SolutionTests : XCTestCase
+
+@end
+
+@implementation NSUserDefaults__Segues_and_Protocols_Challenge_SolutionTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
